43fbe477e178b6d72326098412a53f0649645bdc
rahem sorour <sorourrahem17@gmail.com>
2023-07-22 03:44:51 +0100

    assignement completed

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+if (someValue<=400){
+  return 'This one is on me!'
+}
+else if (someValue>=400 && someValue<=1500){
+  return 'That will be twenty bucks.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (someValue>2000 && someValue<=2500){
+  return 'I will gladly take your thirty bucks.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+else if (someValue>2500){
+  return 'No can do.'
+}
+
+}
+
+function ternaryCheckCity(city){
+  if(city==='NYC') {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go."
+  }
+
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+    break; 
+    case 'not as generous':
+      return 'Thank you.';
+    break;  
+    default:
+      return 'Bye.';
+    
+  }
 }
